07ea9bf61c01f3f3c185e00fe7ba56514389873c
Cameron Earl <developer.cameron@gmail.com>
2018-01-18 00:49:12 -0700

    Add drawing and clear screen

--- gameoflife.js
+++ gameoflife.js
@@ -1,8 +1,13 @@
 class gameOfLife {
-	constructor(size = 100) {
-		this.grid = new Array(size)
-			.fill([])
-			.map(row => new Array(size).fill(0).map(cell => Math.random() > 0.5))
+	constructor(len = 100, empty = false) {
+		this.grid = new Array(len)
+			.fill('')
+			.map(row =>
+				new Array(len)
+					.fill('')
+					.map(cell => new Cell(empty ? false : undefined)),
+			)
+		this.livingCellCount = -1
 	}
 
 	getGrid() {
@@ -11,13 +16,14 @@ class gameOfLife {
 
 	step() {
 		let len = this.grid.length
-		let nextGrid = new Array(len).fill('').map(e => [])
+		let nextGrid = new Array(len).fill('').map(row => [])
+
 		const wrap = n => (n < 0 ? len + n : n % len)
 
 		const getNewCellStatus = (row, col) => {
 			let sum = sumNeighbors(row, col)
 			// console.log('sum', sum, row, col)
-			return sum === 3 || (sum === 2 && this.grid[row][col])
+			return sum === 3 || (sum === 2 && this.grid[row][col].isAlive())
 		}
 
 		const sumNeighbors = (row, col) => {
@@ -25,19 +31,58 @@ class gameOfLife {
 			for (let i = -1; i <= 1; i++) {
 				for (let j = -1; j <= 1; j++) {
 					if (i || j) {
-						sum += this.grid[wrap(row + i)][wrap(col + j)]
+						sum += this.grid[wrap(row + i)][wrap(col + j)].isAlive()
 					}
 				}
 			}
 			return sum
 		}
 
+		// record what next status should be
+		let livingCellCount = 0
 		for (let row = 0; row < len; row++) {
 			for (let col = 0; col < len; col++) {
 				nextGrid[row][col] = getNewCellStatus(row, col)
-				// console.log('update', row, col, nextGrid)
+				livingCellCount += nextGrid[row][col]
 			}
 		}
-		this.grid = nextGrid
+		this.livingCellCount = livingCellCount
+		// set next status
+		for (let row = 0; livingCellCount && row < len; row++) {
+			for (let col = 0; col < len; col++) {
+				this.grid[row][col].setAlive(nextGrid[row][col])
+			}
+		}
+	}
+}
+
+const maxHealth = 400
+
+class Cell {
+	constructor(alive = Math.random() > 0.5) {
+		this.health = maxHealth
+		this.alive = alive
+	}
+
+	setAlive(alive = true) {
+		this.alive = alive
+		this.step()
+	}
+
+	isAlive() {
+		return this.alive
+	}
+
+	step() {
+		this.health = this.alive ? this.health - 1 : maxHealth
+		this.health = Math.min(this.health, maxHealth)
+		if (this.health <= 0) {
+			this.alive = false
+			this.health = 0
+		}
+	}
+
+	valueOf() {
+		return this.alive
 	}
 }
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 let game,
 	canv,
 	ctx,
-	width,
+	cellWidth,
 	size,
 	loop,
 	pauseButton,
 	resetButton,
 	fpsInput,
-	sizeInput
+	sizeInput,
+	mousedown = false
 
 window.onload = () => {
 	canv = document.querySelector('#canv')
 	pauseButton = document.querySelector('button#playbutton')
 	resetButton = document.querySelector('button#resetbutton')
 	stepButton = document.querySelector('button#stepbutton')
+	clearButton = document.querySelector('button#clearbutton')
 	fpsInput = document.querySelector('input#fps')
 	sizeInput = document.querySelector('input#size')
+	cellCount = document.querySelector('span#cellcount')
 
 	ctx = canv.getContext('2d')
 
@@ -25,18 +28,41 @@ window.onload = () => {
 		pause()
 		tick()
 	})
+	clearButton.addEventListener('click', ev => initialize(true))
 	fpsInput.addEventListener('change', changeSpeed)
 	sizeInput.addEventListener('change', initialize)
+
+	canv.addEventListener('click', handleClick)
+	canv.addEventListener('contextmenu', handleClick)
+
+	window.addEventListener('mousedown', ev => (mousedown = true))
+	window.addEventListener('mouseup', ev => (mousedown = false))
+	canv.addEventListener('mousemove', ev => {
+		if (mousedown) handleClick(ev)
+	})
 
 	initialize()
 	play(fps)
 }
 
-function initialize() {
-	pause()
+function handleClick(ev) {
+	// console.log(ev)
+	ev.preventDefault()
+	let alive = ev.which === 1
+	let row = Math.floor(ev.layerY / cellWidth)
+	let col = Math.floor(ev.layerX / cellWidth)
+	let cell = game.getGrid()[row][col]
+	if (cell.isAlive() === alive) return
+	cell.setAlive(alive)
+	drawCell(row, col)
+	console.log(row, col, game.getGrid()[row][col])
+}
+
+function initialize(empty = false) {
+	empty = empty === true ? true : false
 	size = Number(sizeInput.value)
-	game = new gameOfLife(size)
-	width = canv.width / size
+	game = new gameOfLife(size, empty)
+	cellWidth = canv.width / size
 	displayGrid()
 }
 
@@ -51,11 +77,8 @@ function displayGrid() {
 	ctx.beginPath()
 	for (let row = 0; row < grid.length; row++) {
 		for (let col = 0; col < grid.length; col++) {
-			if (grid[row][col]) {
-				let x = width * col
-				let y = width * row
-				ctx.moveTo(x, y)
-				ctx.rect(x, y, width, width)
+			if (grid[row][col].isAlive()) {
+				addCellToPath(row, col)
 			}
 		}
 	}
@@ -63,9 +86,26 @@ function displayGrid() {
 	ctx.fill()
 }
 
+function addCellToPath(row, col) {
+	let x = cellWidth * col
+	let y = cellWidth * row
+	ctx.moveTo(x, y)
+	ctx.rect(x, y, cellWidth, cellWidth)
+}
+
+function drawCell(row, col) {
+	ctx.fillStyle = game.getGrid()[row][col].isAlive() ? '#000000' : '#FFFFFF'
+	ctx.beginPath()
+	addCellToPath(row, col)
+	ctx.closePath()
+	ctx.fill()
+}
+
 function tick() {
-	displayGrid()
 	game.step()
+	displayGrid()
+	cellCount.textContent = game.livingCellCount
+	if (!game.livingCellCount) pause()
 }
 
 function togglePause(ev) {
